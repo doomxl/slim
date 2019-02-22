4ef44954dd5cbfec3a2dfd50ced5fdc423c40497
Fahri DAHMANI <fahri.dahmani@hotmail.fr>
2019-02-22 17:11:07 +0100

    add settings in the app() call

--- public/index.php
+++ public/index.php
@@ -4,10 +4,19 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'user';
+$config['db']['pass']   = 'password';
+$config['db']['dbname'] = 'exampleapp';
+
+$app = new \Slim\App(['settings' => $config]);
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+    $response->getBody()->write("Hello, your name is : $name");
 
     return $response;
 });
